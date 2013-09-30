5443f3b96f18464e139afe445ca464a5fcdaf279
chrisdok <christopher.dokkeberg@gmail.com>
2013-09-30 21:30:39 +0200

    Added activity log
    The log currently shows all locations in the database, displaying
    latitude and longitude

--- src/com/example/lufteapp/ActivityLog.java
+++ src/com/example/lufteapp/ActivityLog.java
@@ -1,15 +1,47 @@
 package com.example.lufteapp;
 
+import java.util.ArrayList;
+
+import android.app.ListActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
-public class ActivityLog extends Activity {
-
+public class ActivityLog extends ListActivity {
+	/*
+	 *  @author http://stackoverflow.com/questions/4540754/add-dynamically-elements-to-a-listview-android
+	 */
+	
+	
+	SQLiteDatabase db;
+	ArrayList<String> list = new ArrayList<String>();
+	ArrayAdapter<String> adapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_activity_log);
+		
+		adapter=new ArrayAdapter<String>(this,
+	            android.R.layout.simple_list_item_1,
+	            list);
+	    
+		setListAdapter(adapter);
+		
+		
+		db = openOrCreateDatabase("gpsDataDB", MODE_PRIVATE,null);
+		
+		Cursor cursor = db.rawQuery("SELECT * FROM gpsDataa", null);
+
+		while(cursor.moveToNext())
+		{
+			String lat = cursor.getString(0);
+			String lon = cursor.getString(1);
+			list.add(lat + " " + lon);
+			adapter.notifyDataSetChanged();
+		}
 	}
 
 	@Override
